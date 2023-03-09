24b2c9b9aed6f297a7bb2b2f5d499c89ccc5dfc8
takashikawanaka <takashi.soccer.a@gmail.com>
2023-03-09 17:38:01 +0900

    Init

---
+++ src/main/java/com/twlone/TwloneApplication.java
@@ -0,0 +1,13 @@
+package com.twlone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwloneApplication.class, args);
+	}
+
+}
